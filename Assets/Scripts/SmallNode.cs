4af482db8b9ee266c9d9af8a9a1bc228de2e56bf
Ayaz Khusnutdinov <khusnut99@gmail.com>
2018-08-23 11:00:15 +0300

    init

---
+++ Assets/Scripts/SmallNode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SmallNode : MonoBehaviour {
+
+    public Color hoverColor;
+    public Vector3 offset;
+
+    private GameObject building;
+    private BuildingBlueprint buildingBlueprint;
+
+    private Renderer rend;
+    private Color startColor;
+
+    public SmallNodeDialog dialog;
+
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+	}
+
+    public Vector3 GetBuildPosition()
+    {
+        return transform.position + offset;
+    }
+
+    public void Build(BuildingBlueprint buildingBlueprint)
+    {
+        this.buildingBlueprint = buildingBlueprint;
+        building = (GameObject)Instantiate(buildingBlueprint.prefab, GetBuildPosition(), Quaternion.identity);
+
+    }
+
+    private void OnMouseDown()
+    {
+        dialog.Show(this);
+    }
+
+
+}
---
+++ Assets/Scripts/SmallNodeDialog.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallNodeDialog : MonoBehaviour {
+
+    private SmallNode target;
+    public GameObject ui;
+
+    public BuildingBlueprint building_1;
+
+    public void Show(SmallNode target)
+    {
+        this.target = target;
+        //transform.position = target.GetBuildPosition();
+        ui.SetActive(true);
+    }
+    
+    public void Hide()
+    {
+        ui.SetActive(false);
+    }
+
+    public void SelectBuilding_1()
+    {
+        target.Build(building_1);
+        Hide();
+        Debug.Log("Select Building 1");
+    
+    }
+}
